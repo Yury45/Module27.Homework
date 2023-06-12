6bb97e974ac0139fe7c05ade9ef2a53e93194e05
Red Jluc <zm.ptz.ru2010@yandex.ru>
2023-06-12 18:43:53 +0500

    1) добавлена тестовая страница и файл скриптов для нее; 2)footer и sidebar вынесены в отдельные файлы; 3) дописаны пути в файле sturtup.cs для внесенных в структуру изменений.

---
+++ LifeSpot/Static/Scripts/script.js
@@ -0,0 +1,22 @@
+﻿
+
+function checkAge() {
+    let age = prompt('Пожалуйста, укажите ваш возраст:')
+    if (age < 18) {
+        alert('Данный ресурс предназначен для лиц достигших 18 лет.')
+        window.location.href = "http://www.google.com";
+    }
+    else alert('Приветсвтуем на LifeSpot!\nСегодня: ' + new Date().toLocaleString());
+}
+
+
+function inputSearching() {
+    let query = document.getElementsByName('searchString')[0].value.toLowerCase();
+    let elements = document.getElementsByClassName('video-container');
+    for (let i = 0; i <= elements.length; i++) {
+        let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
+
+        if (videoText.toLowerCase().includes(query)) elements[i].style.display = 'inline-block';
+        else elements[i].style.display = 'none';
+    }
+}
---
+++ LifeSpot/Static/Scripts/testing.js
@@ -0,0 +1,5 @@
+﻿let name = prompt('Пожалуйста, укажите ваше имя:')
+alert(`Добрый день, ${name}\nДлина имени: ${name.length}`)
+
+let elements = document.getElementsByTagName('*');
+alert(`Количество элементов на странице:  ${elements.length}`);
